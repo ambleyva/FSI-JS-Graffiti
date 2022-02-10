56155a6729f3f31561511d5e78276c3d8eafa053
ambleyva <ambleyva24@gmail.com>
2022-02-09 19:08:07 -0500

    array changes img

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-// Select some elements...
+...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
