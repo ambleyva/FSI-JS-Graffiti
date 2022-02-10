9a00049400786c7df71b5fd9ff3a8e193eb292fd
ambleyva <ambleyva24@gmail.com>
2022-02-09 19:13:29 -0500

    text align

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-...
+
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
+}
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
 }
